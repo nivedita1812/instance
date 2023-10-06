5b43d3c874e2e3fce54c3c8367183a3eaac2c4ec
tom <tom@gmail.com>
2023-10-06 17:08:05 +0000

    adding content

---
+++ module2.java
@@ -0,0 +1,10 @@
+class student{
+  int age;
+  student(int age){
+    this.age=age;
+ }
+public static void main(String[] args){
+   student s=new student(20);
+   system.out.println(s.age);
+}
+}
---
+++ module4.java
@@ -0,0 +1,10 @@
+class teacher{
+  int age;
+  teacher(int age){
+    this.age=age;
+ }
+public static void main(String[] args){
+   teacher t=new teacher(20);
+   system.out.println(t.age);
+}
+}
